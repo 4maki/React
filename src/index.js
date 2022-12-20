ef75214adbb629430aee53e8c8a8e610c9c3a394
Marijan <tomot@tomo.com>
2022-12-20 11:16:29 +0100

    Create app template

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));   //react kreira svoj virutalni dom u kojem je objekt sa dvi instance 'istog'DOM-a,tj kad naide na razliku onda je primjeni
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
